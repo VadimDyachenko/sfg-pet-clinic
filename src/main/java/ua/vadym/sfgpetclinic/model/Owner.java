08173bc1a00a3656e913257938b3a2319a30d703
Vadym <vadim.dyachenko@gmail.com>
2018-11-22 21:19:21 +0200

    #2: Added POJOs

---
+++ src/main/java/ua/vadym/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package ua.vadym.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/ua/vadym/sfgpetclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package ua.vadym.sfgpetclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

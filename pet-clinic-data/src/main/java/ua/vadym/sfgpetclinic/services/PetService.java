db3b5dd46e0849453dba9a5da4c87542a2396b21
Vadym <vadim.dyachenko@gmail.com>
2018-12-04 20:43:57 +0200

    #7 Refactoring to common service interface.

--- pet-clinic-data/src/main/java/ua/vadym/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/ua/vadym/sfgpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package ua.vadym.sfgpetclinic.services;
 
 import ua.vadym.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet Pet);
-
-    Set<Pet> findAll();
 }

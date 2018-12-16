5d41a418a5f2e46a30d8751f8a765f13213f9fc1
Vadym <vadim.dyachenko@gmail.com>
2018-12-16 17:30:40 +0200

    #32 Enhanced owners with contact info and pets on startup.

--- pet-clinic-data/src/main/java/ua/vadym/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/ua/vadym/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -2,12 +2,23 @@ package ua.vadym.sfgpetclinic.services.map;
 
 import org.springframework.stereotype.Service;
 import ua.vadym.sfgpetclinic.model.Owner;
+import ua.vadym.sfgpetclinic.model.Pet;
 import ua.vadym.sfgpetclinic.services.OwnerService;
+import ua.vadym.sfgpetclinic.services.PetService;
+import ua.vadym.sfgpetclinic.services.PetTypeService;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstarctMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -21,7 +32,28 @@ public class OwnerServiceMap extends AbstarctMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner owner) {
-        return super.save(owner);
+
+        if(owner != null) {
+            if(owner.getPets() != null) {
+                owner.getPets().forEach(pet -> {
+                    if(pet.getPetType() != null ) {
+                        if(pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is required.");
+                    }
+
+                    if(pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(owner);
+        } else {
+            return null;
+        }
     }
 
     @Override
--- pet-clinic-web/src/main/java/ua/vadym/sfgpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/ua/vadym/sfgpetclinic/bootstrap/DataInitializer.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import ua.vadym.sfgpetclinic.model.Owner;
+import ua.vadym.sfgpetclinic.model.Pet;
 import ua.vadym.sfgpetclinic.model.PetType;
 import ua.vadym.sfgpetclinic.model.Vet;
 import ua.vadym.sfgpetclinic.services.OwnerService;
 import ua.vadym.sfgpetclinic.services.PetTypeService;
 import ua.vadym.sfgpetclinic.services.VetService;
+
+import java.time.LocalDate;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
@@ -40,10 +43,32 @@ public class DataInitializer implements CommandLineRunner {
         Owner firstOwner = new Owner();
         firstOwner.setFirstName("Jim");
         firstOwner.setLastName("Lovell");
+        firstOwner.setAddress("123 First str.");
+        firstOwner.setCity("Dream City");
+        firstOwner.setTelephone("12341234");
+
+        Pet jimsPet = new Pet();
+        jimsPet.setPetType(savedDogPetType);
+        jimsPet.setOwner(firstOwner);
+        jimsPet.setBirthDate(LocalDate.of(2018, 10,10));
+        jimsPet.setName("Tyson");
+
+        firstOwner.getPets().add(jimsPet);
 
         Owner secondOwner = new Owner();
         secondOwner.setFirstName("Ken");
         secondOwner.setLastName("Mattingly");
+        secondOwner.setAddress("15 Second ave.");
+        secondOwner.setCity("Dream City");
+        secondOwner.setTelephone("55544455");
+
+        Pet kensPet = new Pet();
+        kensPet.setPetType(savedCatPetType);
+        kensPet.setOwner(secondOwner);
+        kensPet.setBirthDate(LocalDate.of(2018, 3, 4));
+        kensPet.setName("Superman");
+
+        secondOwner.getPets().add(kensPet);
 
         ownerService.save(firstOwner);
         ownerService.save(secondOwner);

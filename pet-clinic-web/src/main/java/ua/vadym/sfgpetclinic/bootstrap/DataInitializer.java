8e90711a3046ae14796275b1f8b3edf5ed356c95
Vadym <vadim.dyachenko@gmail.com>
2018-12-16 16:55:43 +0200

    #31 Added loading PetTypes at startup.

--- pet-clinic-web/src/main/java/ua/vadym/sfgpetclinic/bootstrap/DataInitializer.java
+++ pet-clinic-web/src/main/java/ua/vadym/sfgpetclinic/bootstrap/DataInitializer.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import ua.vadym.sfgpetclinic.model.Owner;
+import ua.vadym.sfgpetclinic.model.PetType;
 import ua.vadym.sfgpetclinic.model.Vet;
 import ua.vadym.sfgpetclinic.services.OwnerService;
+import ua.vadym.sfgpetclinic.services.PetTypeService;
 import ua.vadym.sfgpetclinic.services.VetService;
 
 @Component
@@ -13,15 +15,28 @@ public class DataInitializer implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     @Autowired
-    public DataInitializer(OwnerService ownerService, VetService vetService) {
+    public DataInitializer(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType cat = new PetType();
+        cat.setName("Cat");
+
+        PetType savedDogPetType = petTypeService.save(dog);
+        PetType savedCatPetType = petTypeService.save(cat);
+
+        System.out.println("Loaded pet types...");
+
         Owner firstOwner = new Owner();
         firstOwner.setFirstName("Jim");
         firstOwner.setLastName("Lovell");

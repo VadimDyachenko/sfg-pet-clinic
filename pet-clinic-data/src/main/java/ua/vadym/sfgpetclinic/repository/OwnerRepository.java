a90aa5f4c1906f8c1d831271ff5adc85aed34fb1
Vadym Diachenko <vadim.dyachenko@gmail.com>
2019-01-13 16:17:16 +0200

    T-53: Implemented Find Owner functionality.

--- pet-clinic-data/src/main/java/ua/vadym/sfgpetclinic/repository/OwnerRepository.java
+++ pet-clinic-data/src/main/java/ua/vadym/sfgpetclinic/repository/OwnerRepository.java
@@ -3,7 +3,11 @@ package ua.vadym.sfgpetclinic.repository;
 import org.springframework.data.repository.CrudRepository;
 import ua.vadym.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/ua/vadym/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/ua/vadym/sfgpetclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package ua.vadym.sfgpetclinic.services;
 
 import ua.vadym.sfgpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findByLastNameLike(String lastName);
 
 }

13f8500590b8ae4f3edd5982fbdd5d4412ea44a3
Vadym <vadim.dyachenko@gmail.com>
2019-01-20 20:21:34 +0200

    #64 Add Vets JSON endpoint

--- pet-clinic-web/src/main/java/ua/vadym/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/ua/vadym/sfgpetclinic/controllers/VetController.java
@@ -3,8 +3,12 @@ package ua.vadym.sfgpetclinic.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ua.vadym.sfgpetclinic.model.Vet;
 import ua.vadym.sfgpetclinic.services.VetService;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -16,11 +20,16 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson() {
+        return vetService.findAll();
     }
 }
